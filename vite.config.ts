f9ddc82128741d97033641f4e238e90e88792781
gongjunfei <1491180173@qq.com>
2022-12-23 21:41:06 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import vitePluginEslint from 'vite-plugin-eslint'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [uni(), vitePluginEslint()],
+})
